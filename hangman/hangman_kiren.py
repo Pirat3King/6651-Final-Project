313d412f49f15f18d01c81ece7274d792a4bc3bb
pbquade <kiren.chaudry@gmail.com>
2023-11-19 15:07:53 -0500

    Updated main to accept username before starting the games interface.

--- hangman/hangman_kiren.py
+++ hangman/hangman_kiren.py
@@ -9,111 +9,156 @@ Goal: This is a program to run a simple game of hangman
 import random
 import tkinter as tk
 
-# Set up the main window
-window = tk.Tk()
-window.title("Hangman Game")
+class Hangman:
+
+    def __init__(self, main_tkinter):
+        global canvas, attempts_label, letters_guessed_label, letter_label, letter_entry, guess_button, word_label
+        global word_display, message_label, restart_button, exit_button
+        self.window = main_tkinter
+
+        # Initialize variables
+        self.word_to_guess = self.choose_random_word()
+        self.guessed_letters = []
+        self.attempts = 6
+        self.current_attempt = 0
+
+        # Create a canvas to draw the hangman figure
+        canvas = tk.Canvas(self.window, width=200, height=200)
+
+        canvas.create_line(10, 180, 90, 180)
+        canvas.create_line(50, 180, 50, 10)
+        canvas.create_line(50, 10, 100, 10)
+        canvas.create_line(100, 10, 100, 30)
+
+        # Create and pack the widgets
+        attempts_label = tk.Label(self.window, text=f"Attempts left: {self.attempts - self.current_attempt}")
+
+        letters_guessed_label = tk.Label(self.window, text="Letters Guessed: ")
+
+        letter_label = tk.Label(self.window, text="Guess a letter:")
+
+        letter_entry = tk.Entry(self.window)
+
+        guess_button = tk.Button(self.window, text="Guess", command=self.guess_letter)
+
+        word_display = tk.StringVar()
+        word_display.set(" ".join(["_" for _ in self.word_to_guess]))
+        word_label = tk.Label(self.window, textvariable=word_display)
+
+        message_label = tk.Label(self.window, text="")
+
+        restart_button = tk.Button(self.window, text="Restart", command=self.play_again)
+
+        exit_button = tk.Button(self.window, text="Exit", command=self.exit_game)
+
+        # Start the game
+        self.update_hangman()
+
+    # Function to get a random word from wordlist
+    def choose_random_word(self):
+        with open('wordlist.txt', 'r') as file:
+            words = file.read().splitlines()
+        return random.choice(words)
+
+    # Function to update the hangman figure
+    def update_hangman(self):
+        if self.current_attempt == 1:    #head
+            canvas.create_oval(85, 30, 115, 60)
+        elif self.current_attempt == 2:  #body
+            canvas.create_line(100, 60, 100, 120)
+        elif self.current_attempt == 3:  # left arm
+            canvas.create_line(100, 80, 80, 60)
+        elif self.current_attempt == 4:  # right arm
+            canvas.create_line(100, 80, 120, 60)
+        elif self.current_attempt == 5:  # left leg
+            canvas.create_line(100, 120, 80, 140)
+        elif self.current_attempt == 6:  # right leg
+            canvas.create_line(100, 120, 120, 140)
+            # X's inside the circle for dead eyes
+            canvas.create_text(95, 40, text="x")
+            canvas.create_text(105, 40, text="x")
+
+    # Function to check if the game is won
+    def is_game_won(self):
+        return all(letter in self.guessed_letters for letter in self.word_to_guess)
+
+    # Function to handle letter guesses
+    def guess_letter(self):
+        letter = letter_entry.get().lower()
+        self.guessed_letters.append(letter)
+        letter_entry.delete(0, tk.END)
+
+        if letter not in self.word_to_guess:
+            self.current_attempt += 1
+            self.update_hangman()
+
+        word_display.set(" ".join([char if char in self.guessed_letters else "_" for char in self.word_to_guess]))
+
+        # Update attempts label
+        attempts_left = self.attempts - self.current_attempt
+        attempts_label.config(text=f"Attempts left: {attempts_left}")
+
+        # Update letters guessed label
+        letters_guessed_label.config(text=f"Letters Guessed: {' '.join(self.guessed_letters)}")
+
+        if self.is_game_won():
+            message_label.config(text="Congratulations! You've won!")
+        elif self.current_attempt == self.attempts:
+            message_label.config(text=f"Game over! The word was '{self.word_to_guess}'")
+
+    # Function to play again
+    def play_again(self):
+        self.reset_game()
+        unpack_hangman_elements()
+        hangman_game = Hangman(self.window)
+        pack_hangman_elements()
+
+    # Function to exit the game
+    def exit_game(self):
+        self.window.destroy()
+
+    # Function to reset the game state
+    def reset_game(self):
+        self.word_to_guess = self.choose_random_word()
+        self.guessed_letters = []
+        self.attempts = 6
+        self.current_attempt = 0
+        self.update_hangman()
+        word_display.set(" ".join(["_" for _ in self.word_to_guess]))
+        attempts_label.config(text=f"Attempts left: {self.attempts}")
+        letters_guessed_label.config(text="Letters Guessed: ")
+        message_label.config(text="")
+
+    # Run the main loop
+    def run(self):
+        self.window.mainloop()
+
+# Call the Hangman class to start the game
+#hangman_game = Hangman()
+#hangman_game.run()
+
+def pack_hangman_elements():
+    canvas.pack()
+    attempts_label.pack()
+    letters_guessed_label.pack()
+    letter_label.pack()
+    letter_entry.pack()
+    guess_button.pack()
+    word_label.pack()
+    message_label.pack()
+    restart_button.place(relx=0.48, rely=0.56, anchor=tk.CENTER)
+    exit_button.place(relx=0.52, rely=0.56, anchor=tk.CENTER)
+
+def unpack_hangman_elements():
+    canvas.pack_forget()
+    attempts_label.pack_forget()
+    letters_guessed_label.pack_forget()
+    letter_label.pack_forget()
+    letter_entry.pack_forget()
+    guess_button.pack_forget()
+    word_label.pack_forget()
+    message_label.pack_forget()
+    restart_button.place_forget()
+    exit_button.place_forget()
 
 
-# Function to get a random word from wordlist
-def choose_random_word():
-    with open('wordlist.txt', 'r') as file:
-        words = file.read().splitlines()
-    return random.choice(words)
-
-
-# Initialize variables
-word_to_guess = choose_random_word()
-guessed_letters = []  # List to store guessed letters
-attempts = 6  # Number of attempts allowed
-current_attempt = 0  # Current attempt
-
-# Create a canvas to draw the hangman figure
-canvas = tk.Canvas(window, width=200, height=200)
-canvas.pack()
-
-canvas.create_line(10, 180, 90, 180)
-canvas.create_line(50, 180, 50, 10)
-canvas.create_line(50, 10, 100, 10)
-canvas.create_line(100, 10, 100, 30)
-
-
-# Function to update the hangman figure
-def update_hangman():
-    global current_attempt
-    if current_attempt == 1:    #head
-        canvas.create_oval(85, 30, 115, 60)
-    elif current_attempt == 2:  #body
-        canvas.create_line(100, 60, 100, 120)
-    elif current_attempt == 3:  # left arm
-        canvas.create_line(100, 80, 80, 60)
-    elif current_attempt == 4:  # right arm
-        canvas.create_line(100, 80, 120, 60)
-    elif current_attempt == 5:  # left leg
-        canvas.create_line(100, 120, 80, 140)
-    elif current_attempt == 6:  # right leg
-        canvas.create_line(100, 120, 120, 140)
-        # X's inside the circle for dead eyes
-        canvas.create_text(95, 40, text="x")
-        canvas.create_text(105, 40, text="x")
-
-
-# Function to check if the game is won
-def is_game_won():
-    return all(letter in guessed_letters for letter in word_to_guess)
-
-
-# Function to handle letter guesses
-def guess_letter():
-    global current_attempt
-    letter = letter_entry.get()
-    guessed_letters.append(letter)
-    letter_entry.delete(0, tk.END)
-
-    if letter not in word_to_guess:
-        current_attempt += 1
-        update_hangman()
-
-    word_display.set(" ".join([letter if letter in guessed_letters else "_" for letter in word_to_guess]))
-
-    # Update attempts label
-    attempts_left = attempts - current_attempt
-    attempts_label.config(text=f"Attempts left: {attempts_left}")
-
-    # Update letters guessed label
-    letters_guessed_label.config(text=f"Letters Guessed: {' '.join(guessed_letters)}")
-
-    if is_game_won():
-        message_label.config(text="Congratulations! You've won!")
-    elif current_attempt == attempts:
-        message_label.config(text=f"Game over! The word was '{word_to_guess}'")
-
-
-# Create and pack the widgets
-attempts_label = tk.Label(window, text=f"Attempts left: {attempts - current_attempt}")
-attempts_label.pack()
-
-letters_guessed_label = tk.Label(window, text="Letters Guessed: ")
-letters_guessed_label.pack()
-
-letter_label = tk.Label(window, text="Guess a letter:")
-letter_label.pack()
-
-letter_entry = tk.Entry(window)
-letter_entry.pack()
-
-guess_button = tk.Button(window, text="Guess", command=guess_letter)
-guess_button.pack()
-
-word_display = tk.StringVar()
-word_display.set(" ".join(["_" for _ in word_to_guess]))
-word_label = tk.Label(window, textvariable=word_display)
-word_label.pack()
-
-message_label = tk.Label(window, text="")
-message_label.pack()
-
-# Start the game
-update_hangman()
-
-# Run the main loop
-window.mainloop()
--- main.py
+++ main.py
@@ -1,39 +1,65 @@
-from os import system
-import hangman.hangman_trevor as hangman
-import snake.snake_game as snake
-import checkers.checkers as checkers
-system('cls')
-print("Welcome to the Team Pheonix final project for CSCI 6651-01.")
+import tkinter as tk
+from tkinter import simpledialog
+from checkers.checkers import Checkers
+from hangman.hangman_kiren import Hangman, pack_hangman_elements, unpack_hangman_elements
+from snake.snake import Snake
 
-def main_menu():
-    selection = ""
-    the_games = ["1", "2", "3"]
-    print("Make your selection\n1. Hangman\n2. Snake\n3. Checkers")
-    selection = input("> ")
-    while selection not in the_games:
-        print("Invalid Selection")
-        print("Make your selection\n1. Hangman\n2. Snake\n3. Checkers")
-        selection = input("> ")
-    return int(selection)
 
-game_over = False
-while not game_over:
-    game = main_menu()
+game_running = False  # Flag to track whether a game is currently running
 
-    if game == 1:
-        hangman.play_hangman()
-    if game == 2:
-        snake.play_snake()
-    if game == 3:
-        checker_game = checkers.Checkers()
-        checker_game.run()
+# Get the username from the user before starting the game
+username = simpledialog.askstring("Phoenix Games", "Welcome to Phoenix Games! Enter your username:")
 
-    play_again = input("Play again? (y or n): ").upper()
-    while play_again != "Y" and play_again != "N":
-        print("Ivalid Input.")
-        play_again = input("Play again? (y or n): ").upper()
-    if play_again == "Y":
-        pass
-    elif play_again == "N":
-        game_over = True
+root = tk.Tk()
+root.title("Team Phoenix")
+root.geometry("900x700")
+root.state('zoomed') # Defaults to maximized view
 
+# Each function will start the selected game and close all other games
+def play_hangman():
+    pack_hangman_elements()
+    checkers_canvas_widget.pack_forget()
+    snake_canvas_widget.pack_forget()
+
+def play_snake():
+    unpack_hangman_elements()
+    checkers_canvas_widget.pack_forget()
+    snake_canvas_widget.pack(pady=100)
+
+def play_checkers():
+    unpack_hangman_elements()
+    checkers_canvas_widget.pack(pady=100)
+    snake_canvas_widget.pack_forget()
+
+
+radio = tk.IntVar()
+
+#Radio buttons to play each game,
+game1_radio = tk.Radiobutton(root, text="Play Hangman", variable=radio, value=1, command=play_hangman).place(x=50, y=580)
+game2_radio = tk.Radiobutton(root, text="Play Snake", variable=radio, value=2, command=play_snake).place(x=50, y=600)
+game3_radio = tk.Radiobutton(root, text="Play Checkers", variable=radio, value=3, command=play_checkers).place(x=50,y=620)
+
+# Create and pack the username display and attempts widgets
+username_display_label = tk.Label(root, text=f"Username: {username}")
+username_display_label.place(x=50, y=540)
+
+# Entry for the username
+"""username_label = tk.Label(root, text="Enter your username:")
+username_label.place(x=50, y=540)
+
+username_entry = tk.Entry(root)
+username_entry.place(x=200, y=540)"""
+
+checkers_canvas_widget = tk.Canvas(root, width=400, height=400)
+checkers_game = Checkers(checkers_canvas_widget)
+
+snake_canvas_widget = tk.Canvas(root, width=400, height=400, bg="black")
+snake_game = Snake(root,snake_canvas_widget)
+
+# Used to reset the snake game, we need to
+restart_button = tk.Button(root, text="Restart Game", command=snake_game.restart_game).place(x=850,y=800)
+
+# Hangman must be last otherwise it bombs out
+Hangman(root)
+
+root.mainloop()
