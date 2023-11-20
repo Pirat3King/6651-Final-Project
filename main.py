ee8c3cbaa21ac9e6fbf173a74ad5514a9e323b42
pbquade <kiren.chaudry@gmail.com>
2023-11-19 20:00:44 -0500

    Kiren's main file

--- main.py
+++ main.py
@@ -1,92 +1,111 @@
 import tkinter as tk
-from checkers.checkers import Checkers
-from hangman.hangman import Hangman, call_pack,call_unpack
-from snake.snake import Snake
+from tkinter import simpledialog
 import json
-import socket
-import os
+from checkers.checkers import Checkers
+from hangman.hangman_kiren import Hangman, pack_hangman_elements, unpack_hangman_elements
+from snake.snake import Snake
 
-def gen_json_format(username, hm_score, s_score, ch_score):
-    json_format = {
-        username: [
-            {
-                "hangman_score": hm_score,
-                "snake_score": s_score,
-                "checkers_score": ch_score
-            }
-        ]
-    }
-    return json_format
-
-username = socket.gethostname()
-user_scores = "my_high_scores.json"
-json_hangman_score = 0
-json_snake_score = 0
-json_checkers_score = 0
 
 game_running = False  # Flag to track whether a game is currently running
 
-if not os.path.exists(user_scores):
-    with open(user_scores, 'w') as json_file:
-        json.dump(gen_json_format(username, json_hangman_score, json_snake_score, json_checkers_score), json_file, indent=4)
-else:
-    with open(user_scores, 'r') as json_file:
-        data = json.load(json_file)
-        for user in data[username]:
-            json_hangman_score = user['hangman_score']
-            json_snake_score = user['snake_score']
-            json_checkers_score = user['checkers_score']
+# function to handle user data
+def get_user_data(username):
+    try:
+        with open("user_data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        # If the file doesn't exist, create an empty JSON structure
+        data = {"users": []}
 
+    users = data.get("users", [])
+
+    for user in users:
+        if user["username"] == username:
+            return user
+
+    # If the username is not found, create a new user
+    new_user = {"username": username, "hangman_wins": 0, "snake_score": 0, "checkers_wins": 0}
+    users.append(new_user)
+
+    # Update the data dictionary
+    data["users"] = users
+
+    # Save the updated data to the file
+    with open("user_data.json", "w") as file:
+        json.dump(data, file)
+
+    return new_user
+
+
+# function to show the scoreboard
+def show_scoreboard():
+    try:
+        with open("user_data.json", "r") as file:
+            data = json.load(file)
+
+        users = data.get("users", [])
+        scoreboard_text = "\n".join(
+            f"{user['username']}: Hangman Wins - {user['hangman_wins']}, Snake Score - {user['snake_score']}, Checkers Wins - {user['checkers_wins']}"
+            for user in users)
+
+        # Create a custom Toplevel window
+        scoreboard_window = tk.Toplevel(root)
+        scoreboard_window.title("Scoreboard")
+
+        # Create a Label to display the scoreboard text
+        scoreboard_label = tk.Label(scoreboard_window, text=scoreboard_text, padx=20, pady=20)
+        scoreboard_label.pack()
+
+        # Add a button to close the window
+        close_button = tk.Button(scoreboard_window, text="Close", command=scoreboard_window.destroy)
+        close_button.pack()
+    except FileNotFoundError:
+        messagebox.showinfo("Scoreboard", "No scores found.")
+
+# Get the username from the user before starting the game
+username = simpledialog.askstring("Phoenix Games", "Welcome to Phoenix Games! Enter your username:")
+
+user_data = get_user_data(username)
 
 root = tk.Tk()
-root.title("Team Pheonix")
+root.title("Team Phoenix")
 root.geometry("900x700")
 root.state('zoomed') # Defaults to maximized view
 
-
 # Each function will start the selected game and close all other games
 def play_hangman():
-    call_pack()
+    pack_hangman_elements()
     checkers_canvas_widget.pack_forget()
     snake_canvas_widget.pack_forget()
 
 def play_snake():
-    call_unpack()
+    unpack_hangman_elements()
     checkers_canvas_widget.pack_forget()
     snake_canvas_widget.pack(pady=100)
 
 def play_checkers():
-    call_unpack()
+    unpack_hangman_elements()
     checkers_canvas_widget.pack(pady=100)
     snake_canvas_widget.pack_forget()
 
-def reset_game():
-    if radio.get() == 1:
-        pass
-    elif radio.get() == 2:
-        snake_game.restart_game()
-    elif radio.get() == 3:
-        checkers_game.reset_game()
-
-def save_high_scores():
-    global json_snake_score
-    if snake_game.score > json_snake_score:
-        json_snake_score = snake_game.score
-    with open(user_scores, 'w') as json_file:
-        json.dump(gen_json_format(username, json_hangman_score, json_snake_score, json_checkers_score), json_file, indent=4)
-
-
-# def test_func():
-#     print(snake_game.score)
-#     print(json_snake_score)
 
 radio = tk.IntVar()
 
-#Radio buttons to play each game, 
-game1_radio = tk.Radiobutton(root, text="Play Hangman", variable=radio, value=1, command=play_hangman).place(x=650, y=800)
-game2_radio = tk.Radiobutton(root, text="Play Snake", variable=radio, value=2, command=play_snake).place(x=650, y=820)
-game3_radio = tk.Radiobutton(root, text="Play Checkers", variable=radio, value=3, command=play_checkers).place(x=650,y=840)
+#Radio buttons to play each game,
+game1_radio = tk.Radiobutton(root, text="Play Hangman", variable=radio, value=1, command=play_hangman).place(x=50, y=580)
+game2_radio = tk.Radiobutton(root, text="Play Snake", variable=radio, value=2, command=play_snake).place(x=50, y=600)
+game3_radio = tk.Radiobutton(root, text="Play Checkers", variable=radio, value=3, command=play_checkers).place(x=50,y=620)
 
+# Create and pack the username display and attempts widgets
+username_display_label = tk.Label(root, text=f"Username: {username}")
+username_display_label.place(x=50, y=540)
+
+# Entry for the username
+"""username_label = tk.Label(root, text="Enter your username:")
+username_label.place(x=50, y=540)
+
+username_entry = tk.Entry(root)
+username_entry.place(x=200, y=540)"""
 
 checkers_canvas_widget = tk.Canvas(root, width=400, height=400)
 checkers_game = Checkers(root, checkers_canvas_widget)
@@ -94,13 +113,14 @@ checkers_game = Checkers(root, checkers_canvas_widget)
 snake_canvas_widget = tk.Canvas(root, width=400, height=400, bg="black")
 snake_game = Snake(root,snake_canvas_widget)
 
-# Used to reset snake or checkers, issues with checkers
-restart_button = tk.Button(root, text="Restart Game", command=reset_game).place(x=850,y=800)
-save_button = tk.Button(root, text="Save High Scores", command=save_high_scores).place(x=850,y=830)
-# test_button = tk.Button(root, text="Test Button", command=test_func).place(x=850,y=830)
+# Used to reset the snake game, we need to
+restart_button = tk.Button(root, text="Restart Game", command=snake_game.restart_game).place(x=850,y=800)
 
+# Button to show scoreboard
+scoreboard_button = tk.Button(root, text="Show Scoreboard", command=show_scoreboard)
+scoreboard_button.place(relx=.95, rely=.95, anchor=tk.SE)
 
 # Hangman must be last otherwise it bombs out
-Hangman(root)
+Hangman(root, user_data)
 
 root.mainloop()
